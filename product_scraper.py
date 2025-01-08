297d76a640be5c2591e55b6f1d693f1a637472c3
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-01-09 00:04:16 +0330

    Refactor product search URL construction to use updated query parameter

--- product_scraper.py
+++ product_scraper.py
@@ -35,22 +35,7 @@ def get_data(page_url):
 			
 	return data
 
-
-suggest_url = f'https://api.torob.com/suggestion2/?q={input_query}'
-suggest_res = requests.get(suggest_url)
-suggest_res_json = suggest_res.json()
-category_list = []
-for suggest in suggest_res_json:
-	if 'category' in suggest.keys():
-		category_list.append(suggest)
-count = 0
-for category in category_list:
-	print(f"{count} - {category['category']['title']}")
-	count+=1
-category_index = int(input('Which category? : '))
-category_id = category_list[category_index]['category']['id']
-
-url = f"https://api.torob.com/v4/base-product/search/?page=0&sort=popularity&size=24&q={input_query}&discover_method=search_autocomplete&category={category_id}&source=next_desktop"
+url = f"https://api.torob.com/v4/base-product/search/?page=0&sort=popularity&size=24&query={input_query}&q={input_query}&source=next_desktop"
 
 full_data = get_data(url)
 if full_data :
